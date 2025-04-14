df21709f289ac6b9b1a292dbe488460356a409b3
lynxwiltner <lynx@wiltner.net>
2025-04-14 11:36:53 +0200

    add OrderStatus and adjust tests and classes

--- src/main/java/ProductRepo.java
+++ src/main/java/ProductRepo.java
@@ -2,37 +2,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductRepo {
-    private List<Product> products;
-
+    private final List<Product> products;
+    
     public ProductRepo() {
         products = new ArrayList<>();
-        products.add(new Product("1", "Apfel"));
+        products.add( new Product( "1", "Apfel" ) );
     }
-
+    
     public List<Product> getProducts() {
         return products;
     }
-
-    public Product getProductById(String id) {
-        for (Product product : products) {
-            if (product.id().equals(id)) {
+    
+    public Product getProductById( String id ) {
+        for ( Product product : products ) {
+            if ( product.id().equals( id ) ) {
                 return product;
             }
         }
         return null;
     }
-
-    public Product addProduct(Product newProduct) {
-        products.add(newProduct);
+    
+    public Product addProduct( Product newProduct ) {
+        products.add( newProduct );
         return newProduct;
     }
-
-    public void removeProduct(String id) {
-        for (Product product : products) {
-           if (product.id().equals(id)) {
-               products.remove(product);
-               return;
-           }
+    
+    public void removeProduct( String id ) {
+        for ( Product product : products ) {
+            if ( product.id().equals( id ) ) {
+                products.remove( product );
+                return;
+            }
         }
     }
 }
--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -3,22 +3,22 @@ import java.util.List;
 import java.util.UUID;
 
 public class ShopService {
-    private ProductRepo productRepo = new ProductRepo();
-    private OrderRepo orderRepo = new OrderMapRepo();
-
-    public Order addOrder(List<String> productIds) {
+    private final ProductRepo productRepo = new ProductRepo();
+    private final OrderRepo orderRepo = new OrderMapRepo();
+    
+    public Order addOrder( List<String> productIds, OrderStatus orderStatus ) {
         List<Product> products = new ArrayList<>();
-        for (String productId : productIds) {
-            Product productToOrder = productRepo.getProductById(productId);
-            if (productToOrder == null) {
-                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
+        for ( String productId : productIds ) {
+            Product productToOrder = productRepo.getProductById( productId );
+            if ( productToOrder == null ) {
+                System.out.println( "Product mit der Id: " + productId + " konnte nicht bestellt werden!" );
                 return null;
             }
-            products.add(productToOrder);
+            products.add( productToOrder );
         }
-
-        Order newOrder = new Order(UUID.randomUUID().toString(), products);
-
-        return orderRepo.addOrder(newOrder);
+        
+        Order newOrder = new Order( UUID.randomUUID().toString(), products, orderStatus );
+        
+        return orderRepo.addOrder( newOrder );
     }
 }
